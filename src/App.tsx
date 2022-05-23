31b7c5dc867b6a23b3affd14728ab0cf1fd0d8a4
callmeabhi1997 <callmeabhi1997@gmail.com>
2022-05-23 15:18:36 +0530

    Part 1 Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useList } from './hooks/useList';
+import { useCounter } from './hooks/useCounter';
+import { v4 as uuid } from 'uuid';
 
 function App() {
+  // Entire thing bellow should work. you don't have to edit this file, only implement  hooks
+  const [list, { push, pop, clear, reset, map }]: any = useList([1, 2, 3]);
+
+  const { value, inc, dec, set } = useCounter(10);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button onClick={() => push(list.length + 1)}>Append</button>
+      <button onClick={() => pop()}>pop</button>
+      <button onClick={() => clear()}>clear</button>
+      <button onClick={() => reset()}>reset</button>
+      <button onClick={() => map((el: any) => el * 2)}>
+        Double every element
+      </button>
+      {list.map((l: any) => (
+        <div key={uuid()}>{l}</div>
+      ))}
+
+      <hr />
+      <button onClick={() => inc()}>Add 1</button>
+      <button onClick={() => inc(3)}>Add 3</button>
+      <button onClick={() => dec()}>Sub 1</button>
+      <button onClick={() => dec(4)}>Sub 4</button>
+      <button onClick={() => set(100)}>Set counter to 100</button>
+      <h3>Value: {value}</h3>
     </div>
   );
 }
